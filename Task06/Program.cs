5d01645174c9ede16eb3fc79010f237ad0a93b28
VallasPetr <vallaspetr@gmail.com>
2023-03-12 23:20:22 +0300

    create  task06

---
+++ Task06/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// Example:
+// 4->да
+// 3->нет
+// 7->нет
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 2;
+if(a==0) 
+{
+    Console.Write($"Число {number} чётное");
+}
+else
+{    
+Console.Write($"Число {number} не чётное");
+}
