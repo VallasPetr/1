0f197339c6a97182a306bd838f221c5641c08ca3
VallasPetr <vallaspetr@gmail.com>
2023-03-15 02:44:11 +0300

    create task11

---
+++ Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и
+//  удаляет вторую цифру этого числа. 
+//  456 -> 46 
+//  782 -> 72 
+//  918 -> 98
+
+int number = new Random().Next(10,1000);
+Console.WriteLine($"Случайное число из отрезка 10 - 999 -> {number}"); 
+
+// int firstDigit = number/100;
+// int thirdDigit = number%10;
+// int result = firstDigit*10 + thirdDigit;
+// Console.WriteLine($"Двузначное число -> {result}");
+
+// Второй вариант
+
+int maxDigit = MaxDigit(number); // Вот сюда возвращает значение
+Console.WriteLine($"Двузначное число -> {maxDigit}");
+int MaxDigit(int num) // num = number, значение number перердаются в num
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit*10 + thirdDigit;
+    return result; // возвращает знвчение в MaxDigit
+}
