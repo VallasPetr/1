dd09caf10e0edd3acf91d8881a963c44380712af
VallasPetr <vallaspetr@gmail.com>
2023-03-20 03:03:04 +0300

    create task12 and task14

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -8,6 +8,7 @@ Console.Write("Введите первое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 // Первый способ
 if(number1%number2==0)
 {
@@ -16,8 +17,9 @@ if(number1%number2==0)
 else
 {
     int number3 = number1%number2;
-    Console.WriteLine($"НЕ КРАТНО {number3}");
+    Console.WriteLine($"НЕ КРАТНО {number3} ");
 }
 
 // Второй способ
+
 
---
+++ Task14/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+// Первый способ 
+
+// int a = number;
+// if(number % 7 == 0 && number %23 == 0)
+
+// {
+//     Console.Write("КРАТНО");
+// }
+// else
+// {
+//     Console.Write("НЕ КРАТНО");
+// }
+
+// Второй способ
+
+bool checkNumber = CheckNumber(number);
+Console.WriteLine(checkNumber ? "КРАТНО" : "НЕ КРАТНО");
+bool CheckNumber(int num)
+
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
