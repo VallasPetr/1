531dbd5bac696afbdf025cc72b9f5381f079c023
VallasPetr <vallaspetr@gmail.com>
2023-03-27 03:30:17 +0300

    create task30

---
+++ Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке
+
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+
+FillArray(array);
+WriteArray(array);
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,2);
+    }
+}
+
+void WriteArray(int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+ " ");
+    }
+}
