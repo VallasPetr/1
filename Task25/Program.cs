d3ff8913bb79413367cfbc6c2b75de607762a623
VallasPetr <vallaspetr@gmail.com>
2023-03-27 19:51:50 +0300

    create task25, task27, task29

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа(А и В) и 
+// возводит число А в натуральную степень В.
+
+// 3, 5 -> 243;
+// 2, 4 -> 16;
+
+Console.WriteLine("Введите число A = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число B = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int expon = Expon(number);
+Console.WriteLine($"A в степени В = {expon}");
+int Expon(int num)
+{
+while(i<=numberB)
+{
+    num *=numberA;
+    i++;
+}
+    random num;
+}
